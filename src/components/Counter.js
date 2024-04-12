cf9ade078645ac2af47ec9f8b25f7b88beef28c8
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-04-12 05:08:38 +0000

    update

---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  return (
+    <main className={classes}>
+      <h1>Redux Counter</h1>
+    </main>
+  );
+};
+
+export default Counter;
