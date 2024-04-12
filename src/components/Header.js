2044895f95d99085e7594b8013c2c028c51deca7
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-04-12 06:23:04 +0000

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
-import calsses from "./Header.module.css";
+import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <h1>Redux Auth</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">My Products</a>
+          </li>
+          <li>
+            <a href="/">My Sales</a>
+          </li>
+          <li>
+            <button>Logout</button>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
