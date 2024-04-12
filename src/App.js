78faf8bb9f594d86b08e76e2ac426738e5cb72f5
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-04-12 07:34:41 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Counter from "./components/Counter";
 import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <>
       <Counter />
       <Header />
+      <Auth />
+      <UserProfile />
     </>
   );
 }
